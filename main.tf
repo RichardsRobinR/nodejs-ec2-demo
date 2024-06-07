73764a3dc81b0da91424868fdbf1e661c51c2e78
Richards Robin R <richardsrobin.r15@gmail.com>
2024-06-07 20:16:27 +0530

    upload main tf file

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-bucket-002"
+    key    = "terraform-bucket-002.tfstate"
+    region = var.region
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ff72998b38e59e40"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServerInstance"
+  }
+}
